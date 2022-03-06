ebc649d86cf0f5ef9a94707e5c9d9476f2961c66
kobeydon <pacemaking313gmail.com>
2022-03-06 17:40:04 +0900

    written to the sheet succeeded

--- timi.py
+++ timi.py
@@ -1,4 +1,5 @@
 import logging
+from tkinter.tix import COLUMN
 from openpyxl import load_workbook
 
 
@@ -15,20 +16,27 @@ class timemanager:
     def edit_timecard(self):
         self.logger.info("edit_timecard start")
 
-        rs = '5'
-        cs = '9'
+        base_row = 8
+        arrive_base_col = 6
+        depart_base_col = 7
         
         wb = load_workbook(filename = self.outputfile)
         #always select first sheet in workbook
         ws = wb.worksheets[0]
+
+        # self.logger.info(self.data)
         
-        row = ws['C']
-        self.logger.info(self.data)
-        self.logger.info(row)
-        
-        for dat in self.data:
-            row[rs] = dat['出社時刻']
-            self.logger.info(row[rs].values())
+        # insert arrival time
+        for dat in self.data['data']:
+            mycell = ws.cell(row = base_row, column= arrive_base_col)
+            mycell.value = dat.get('出社時刻')
+            self.logger.info(dat.get('出社時刻'))
+
+            mycell = ws.cell(row = base_row, column = depart_base_col)
+            mycell.value = dat.get('退社時刻')
+            self.logger.info(dat.get('退社時刻'))
+
+            base_row += 1
 
         # write_to_outputfile
-        wb.save(filename = self.outputfile)
+        wb.save(filename = 'saved.xlsx')
