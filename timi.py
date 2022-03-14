2cad7c3851d9dffd0d03a690bc4d50a303d58d53
kobeydon <pacemaking313@gmail.com>
2022-03-15 08:43:14 +0900

    add convert to excel ordinal

--- timi.py
+++ timi.py
@@ -1,5 +1,5 @@
 import logging
-from tkinter.tix import COLUMN
+from datetime import date
 from openpyxl import load_workbook
 
 
@@ -25,18 +25,32 @@ class timemanager:
         ws = wb.worksheets[0]
 
         # self.logger.info(self.data)
-        
+
         # insert arrival time
         for dat in self.data['data']:
             mycell = ws.cell(row = base_row, column= arrive_base_col)
-            mycell.value = dat.get('出社時刻')
+            atime = datetime.strptime(dat.get('出社時刻'), '%H:%M')
+            mycell.value = self.convert_strtime_to_excel_ordinal(dtime)
             self.logger.info(dat.get('出社時刻'))
 
             mycell = ws.cell(row = base_row, column = depart_base_col)
-            mycell.value = dat.get('退社時刻')
+            dtime = datetime.strptime(dat.get('退社時刻'), '%H:%M')
+            mycell.value = self.convert_date_to_excel_ordinal(atime)
             self.logger.info(dat.get('退社時刻'))
 
             base_row += 1
 
         # write_to_outputfile
         wb.save(filename = 'saved.xlsx')
+
+    def convert_date_to_excel_ordinal(self, targt_time):
+
+        # Specifying offset value i.e.,
+        # the date value for the date of 1900-01-00
+        offset = 693594
+
+        # Calling the toordinal() function to get
+        # the excel serial date number in the form
+        # of date values
+        n = targt_time.toordinal()
+        return (n - offset)
